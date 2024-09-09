eb2eadeaf8e42fb1eb5a85bb5b8353f0703ad0a8
Patrick Boutin <patrick.boutin@roberthalf.com>
2024-09-09 01:46:50 -0700

    Added reducer function with initial action IF blocks for expected add/remove action types.

--- src/store/CartContext.jsx
+++ src/store/CartContext.jsx
@@ -1,4 +1,4 @@
-import { createContext } from 'react';
+import { createContext, useReducer } from 'react';
 
 const CartContext = createContext({
   items: [],
@@ -6,7 +6,21 @@ const CartContext = createContext({
   removeItem: (id) => {}
 });
 
+function cardReducer(state, action) {
+  if (action.type === 'ADD_ITEM') {
+    // ... update the state to add a new item
+  }
+
+  if (action.type === 'REMOVE_ITEM') {
+    // ... update the state to remove an item
+  }
+
+  return state;
+}
+
 export function CartContextProvider({ children }) {
+  useReducer();
+
   return <CartContext.Provider>{children}</CartContext.Provider>;
 }
 
