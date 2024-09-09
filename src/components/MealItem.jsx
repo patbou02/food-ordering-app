23f2b835b3130017e72af34e9088e5ccef7f42d8
Patrick Boutin <patrick.boutin@roberthalf.com>
2024-09-08 17:47:22 -0700

    Created utility function inside formatting.js file to be used to format various data throughout the Application. Refactored MealItem component to leverage formatting function instead of using hard coded $ symbol.

--- src/components/MealItem.jsx
+++ src/components/MealItem.jsx
@@ -1,3 +1,5 @@
+import { currencyFormatter } from '../util/formatting.js';
+
 export default function MealItem({ meal }) {
   return (
     <li className="meal-item">
@@ -6,7 +8,7 @@ export default function MealItem({ meal }) {
         <div>
           <h3>{meal.name}</h3>
           <p className="meal-item-description">{meal.description}</p>
-          <p className="meal-item-price">$${meal.price}</p>
+          <p className="meal-item-price">{currencyFormatter.format(meal.price)}</p>
         </div>
         <p className="meal-item-actions">
           <button>Add to Cart</button>
